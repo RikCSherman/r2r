d0d7e8761b6c43a0e3c98033ee0141c02d06b3ba
Richard Sherman <richardsherman@Richards-MacBook-Pro.local>
2018-02-02 10:24:50 +0000

    UI version

--- src/main/java/r2r/R2r.java
+++ src/main/java/r2r/R2r.java
@@ -1,103 +1,38 @@
 package r2r;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import r2r.converter.SystemMapper;
-import r2r.model.Planet;
-import r2r.model.RtoRSystem;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.io.IOException;
 
 
-public class R2r {
+public class R2r extends Application {
 
-    private static List<RtoRSystem> r2RSystems = new ArrayList<>();
-    private static Map<String, RtoRSystem> allSystems;
-    private static List<String> visited = new ArrayList<>();
-    private static Config config;
-
-    public static void main(String[] args) throws Exception {
-        config = new Config(args);
-        loadAllSystems();
-        loadVisited();
-        loadR2RSystems();
-        doR2R();
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    private static void loadAllSystems() throws IOException {
-        allSystems = new HashMap<>();
-        String fileName = "systems-short.csv";
-        if (!new File(fileName).exists()) {
-            fileName = "systems.csv";
-        }
-        BufferedReader in = new BufferedReader(new FileReader(fileName));
-        String line;
-        in.readLine();
-        while((line = in.readLine()) != null) {
-            RtoRSystem system = new RtoRSystem(line);
-            allSystems.put(system.getName(), system);
-        }
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/r2rgui.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    private static void loadR2RSystems() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode node = mapper.readTree(new BufferedReader(new FileReader("expl_1000 (1).json")));
-        Iterator<Entry<String, JsonNode>> it = node.fields();
-        while (it.hasNext()) {
-            RtoRSystem system = SystemMapper.map(it.next());
-            r2RSystems.add(system);
-        }
-    }
+//    public static void main(String[] args) throws Exception {
+//        config = new Config(args);
+//        loadAllSystems();
+//        loadVisited();
+//        loadR2RSystems();
+//        doR2R();
+//    }
 
-    private static void loadVisited() {
-        try (Stream<String> stream = Files.lines(Paths.get("visited.txt"))) {
-            stream.forEach(visited::add);
-        } catch (IOException e) {
-            // Do nothing
-        }
-    }
 
-    private static void doR2R() {
-        RtoRSystem startPoint = allSystems.get(config.getStartSystem());
-        List<RtoRSystem> localSystems = filterSystems(startPoint, config.getMaxDistance());
-        List<RtoRSystem> route = new ArrayList<>();
-        RtoRSystem first = startPoint;
-        for (int i = 0; i < config.getNoSystems() && localSystems.size() > 0; i++) {
-            first = findNearest(localSystems, first);
-            route.add(first);
-            localSystems.remove(first);
-        }
-        for(RtoRSystem system : route) {
-            System.out.println(system.getName());
-            Collections.sort(system.getPlanets());
-            for (Planet planet : system.getPlanets()) {
-                System.out.println(" - " + planet.getName() + "  :  " + planet.getType());
-            }
-        }
-    }
-
-    private static RtoRSystem findNearest(List<RtoRSystem> localSystems, RtoRSystem startPoint) {
-        RtoRSystem nearest = null;
-        double shortest = Double.MAX_VALUE;
-        for (RtoRSystem system : localSystems) {
-            double distance = system.distance(startPoint);
-            if (distance < shortest) {
-                nearest = system;
-                shortest = distance;
-            }
-        }
-        return nearest;
-    }
-
-    private static List<RtoRSystem> filterSystems(final RtoRSystem startPoint, final int maxDistance) {
-        return r2RSystems.stream()
-                .filter(a -> startPoint.distance(a) <= maxDistance && !visited.contains(a.getName()))
-                .collect(Collectors.toList());
-    }
 }
--- src/main/java/r2r/gui/RtoRController.java
+++ src/main/java/r2r/gui/RtoRController.java
@@ -1,17 +1,114 @@
 package r2r.gui;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import r2r.converter.SystemMapper;
+import r2r.model.Planet;
+import r2r.model.RtoRSystem;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RtoRController {
 
+    private List<RtoRSystem> r2RSystems = new ArrayList<>();
+    private Map<String, RtoRSystem> allSystems;
+    private List<String> visited = new ArrayList<>();
+
     @FXML
-    TextArea route;
+    private TextArea route;
+    @FXML
+    private TextField startSystem;
+
+    @FXML
+    public void initialize() throws Exception {
+        loadAllSystems();
+        loadVisited();
+        loadR2RSystems();
+    }
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        // Button was clicked, do something...
-        route.appendText("Hello Steve\n");
+        doR2R();
+    }
+    private void loadAllSystems() throws IOException {
+        allSystems = new HashMap<>();
+        String fileName = "systems-short.csv";
+        if (!new File(fileName).exists()) {
+            fileName = "systems.csv";
+        }
+        BufferedReader in = new BufferedReader(new FileReader(fileName));
+        String line;
+        in.readLine();
+        while ((line = in.readLine()) != null) {
+            RtoRSystem system = new RtoRSystem(line);
+            allSystems.put(system.getName(), system);
+        }
+    }
+
+    private void loadR2RSystems() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(new BufferedReader(new FileReader("expl_1000 (1).json")));
+        Iterator<Map.Entry<String, JsonNode>> it = node.fields();
+        while (it.hasNext()) {
+            RtoRSystem system = SystemMapper.map(it.next());
+            r2RSystems.add(system);
+        }
+    }
+
+    private void loadVisited() {
+        try (Stream<String> stream = Files.lines(Paths.get("visited.txt"))) {
+            stream.forEach(visited::add);
+        } catch (IOException e) {
+            // Do nothing
+        }
+    }
+    private void doR2R() {
+        RtoRSystem startPoint = allSystems.get(startSystem.getText());
+        List<RtoRSystem> localSystems = filterSystems(startPoint, 100);
+        List<RtoRSystem> routeSystems = new ArrayList<>();
+        RtoRSystem first = startPoint;
+        for (int i = 0; i < 10 && localSystems.size() > 0; i++) {
+            first = findNearest(localSystems, first);
+            routeSystems.add(first);
+            localSystems.remove(first);
+        }
+        for (RtoRSystem system : routeSystems) {
+            route.appendText(system.getName() + "\n");
+            Collections.sort(system.getPlanets());
+            for (Planet planet : system.getPlanets()) {
+                route.appendText(" - " + planet.getName() + "  :  " + planet.getType() + "\n");
+            }
+        }
+    }
+
+    private RtoRSystem findNearest(List<RtoRSystem> localSystems, RtoRSystem startPoint) {
+        RtoRSystem nearest = null;
+        double shortest = Double.MAX_VALUE;
+        for (RtoRSystem system : localSystems) {
+            double distance = system.distance(startPoint);
+            if (distance < shortest) {
+                nearest = system;
+                shortest = distance;
+            }
+        }
+        return nearest;
+    }
+
+    private List<RtoRSystem> filterSystems(final RtoRSystem startPoint, final int maxDistance) {
+        return r2RSystems.stream()
+                .filter(a -> startPoint.distance(a) <= maxDistance && !visited.contains(a.getName()))
+                .collect(Collectors.toList());
     }
 }
